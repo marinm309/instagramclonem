a176cb351c36aa1c46e95d0df4819fce1dff6683
marinm309 <marinm309@abv.bg>
2022-06-07 22:35:56 +0300

    koza

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'instagram_clone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-
+'''''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -94,13 +94,15 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+'''''
 
-#DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-#}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
