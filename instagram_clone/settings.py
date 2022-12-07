6206553905c10af07f68b516e005d6f311ffb93f
marinm309 <marinm309@abv.bg>
2022-12-07 07:19:47 +0200

    asdf

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -86,25 +86,25 @@ WSGI_APPLICATION = 'instagram_clone.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'instagramclonem',
+#         'USER': 'marinm309',
+#         'PASSWORD': 'krfjrj4o0',
+#         'HOST': 'database-1.cfufzhjzmbbp.eu-central-1.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'instagramclonem',
-        'USER': 'marinm309',
-        'PASSWORD': 'krfjrj4o0',
-        'HOST': 'database-1.cfufzhjzmbbp.eu-central-1.rds.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-
-#DATABASES = {
-#    'default': {
- #       'ENGINE': 'django.db.backends.sqlite3',
-  #      'NAME': BASE_DIR / 'db.sqlite3',
-   # }
-#}
 
 
 # Password validation
